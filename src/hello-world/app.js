2e3316f91f1b22044641f7a88145f54bf8b8a6be
VincentBel <buaazqh@gmail.com>
2017-01-18 16:07:01 +0800

    feat(hello-world): add greet button

--- src/hello-world/app.js
+++ src/hello-world/app.js
@@ -2,7 +2,13 @@ import { createClient } from 'service-mocker/client';
 
 const client = createClient('server.js');
 
-client.ready.then(async () => {
-  const response = await fetch('/greet');
-  document.getElementById('helloWorld').innerHTML = await response.text();
-});
+client.ready.then(initApp);
+
+function initApp() {
+  async function greet() {
+    const response = await fetch('/greet');
+    document.getElementById('helloWorld').innerHTML = await response.text();
+  }
+
+  document.getElementById('greet').addEventListener('click', greet);
+}
